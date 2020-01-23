12bf8eb9e0955c1fcb6df3185b689a69a0e5781d
Abdulkaremm <a.karemma95@gmail.com>
2020-01-23 19:36:00 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/invinityapp/inventoryitems/InfinityDB.java

--- app/src/main/java/com/example/invinityapp/registration_of_deficiencies/RegistrationOfDeficiencies.java
+++ app/src/main/java/com/example/invinityapp/registration_of_deficiencies/RegistrationOfDeficiencies.java
@@ -13,6 +13,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
